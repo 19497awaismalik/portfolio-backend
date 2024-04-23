296642238e222dbb938fdec7fb411298609c8b88
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-04-23 21:26:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3006;
 
 server.options("",cors({
-    origin:"http://locahost:5173",
+    origin:"https://portfolio-frontend-three-steel.vercel.app",
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }))
 server.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://portfolio-frontend-three-steel.vercel.app",
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }))
